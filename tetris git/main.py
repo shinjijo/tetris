08a554551c6b4bc5529640851ce9e02c875a0d6b
joel <jo.nadal2015@hotmail.it>
2019-07-15 21:03:09 +0200

    3

--- tetris git/main.py
+++ tetris git/main.py
@@ -1,5 +1,7 @@
 import pygame
+import pygame.locals
 pygame.init()
+
 
 #variabili display
 win = pygame.display.set_mode((500,650))
@@ -16,14 +18,34 @@ def griglia():
             else:
                 print("saasfasfas")
                 pygame.draw.rect(win, bianco , (20+(a*20),40+(b*20),20,20), 1)
+                
+#loop
+def giocoFunzione():
+    inFunzione = True
+    print (inFunzione)
+    while inFunzione == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                inFunzione = False
+                
+        print("saccodimerda")
+    pygame.quit()
+    quit()
+        
+    
 #colori
 rosso = (255,0,0)
-bianco = (0,255,0)
+bianco = (0,255,255)
 
-#campo 
-
+    
+    
+#campo
 griglia()
-pygame.draw.line(win, rosso, (20,40), (20,440), 2)
-pygame.draw.line(win, rosso, (20,440), (220,440), 2)
-pygame.draw.line(win, rosso, (220,40), (220,440), 2)
+pygame.draw.line (win, rosso, (20,40), (20,440), 2)
+pygame.draw.line (win, rosso, (20,440), (220,440), 2)
+pygame.draw.line (win, rosso, (220,40), (220,440), 2)
 pygame.display.update()
+giocoFunzione()
+
+
+
